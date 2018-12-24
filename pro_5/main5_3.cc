65b6d6d28f18d9542d03e9eaebc507d450ac89de
chuacon <huaconchristian@gmail.com>
2018-12-24 15:44:26 -0500

    Project 5

---
+++ pro_5/main5_3.cc
@@ -0,0 +1,16 @@
+//Example program : main5_3.cc
+#include "p5.h"
+int main ()
+{
+	MATRIX X(2,2, "in_5_31"), Y(2,2, "in_5_32");
+
+	X.PRINT();
+	
+	X += Y;
+	X *= Y;
+	
+	Y.PRINT();
+	X.PRINT();
+	
+	return 0;
+}
